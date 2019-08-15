846036adf5d936debdf6d63f9a4f4d8d4bbdc6c1
CanuteTheGreat <jake41397@gmail.com>
2019-08-15 12:03:42 -0400

    Initial Commit

---
+++ Assets/Scripts/HighScoreText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class HighScoreText : MonoBehaviour
+{
+    Text HighScore;
+
+    void OnEnable()
+    {
+        HighScore = GetComponent<Text>();
+        HighScore.text = "Highscore: " + PlayerPrefs.GetInt("HighScore").ToString();
+    }
+}
---
+++ Assets/Scripts/TapController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour
+{
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
+
+    public float tapForce = 10;
+    public Vector3 startPosition;
+    Rigidbody2D rigidBody;
+
+    GameManager game;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.simulated = false;
+        transform.localPosition = startPosition;
+        game = GameManager.Instance;
+    }
+
+    void OnEnable()
+    {
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+
+    void OnDisable()
+    {
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameStarted()
+    {
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.simulated = true;
+    }
+
+    void OnGameOverConfirmed()
+    {
+        transform.localPosition = startPosition;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidBody.velocity = Vector3.zero;
+            rigidBody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "DeathZone")
+        {
+            // Freeze the player
+            rigidBody.simulated = false;
+
+            // Register a dead event
+            OnPlayerDied(); // event sent to gamemanager
+
+            // Play a sound
+
+        }
+        else if (col.gameObject.tag == "ScoreZone")
+        {
+            // register a score event
+            OnPlayerScored(); // event sent to GameManager
+            
+            // Play a sound
+        }
+    }
+}
